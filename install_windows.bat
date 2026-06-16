26707a1c3049cb1cfb11051c2f620585e35f9dcd
Jeelan Madakiaya <jeelan.madakiya26@gmail.com>
2026-06-16 20:22:26 +0530

    Update install_windows.bat

--- install_windows.bat
+++ install_windows.bat
@@ -5,3 +5,5 @@ python -m pip install --upgrade pip
 pip install -r requirements.txt
 if not exist .env copy .env.example .env
 echo Installation complete. Start Redis, then run run_windows.bat
+passlib==1.7.4
+bcrypt==4.0.1
